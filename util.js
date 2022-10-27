44b390a8609a6862df5da333902f4e7a3c5ab8bd
Leontin Kremer <kremer.leontin@gmail.com>
2022-10-27 07:25:33 +0200

    Create util.js

---
+++ util.js
@@ -0,0 +1,13 @@
+const carModel = {
+  manufacturer: "VW",
+  modell: "Golf",
+};
+
+const entriesArray = Object.entries(carModel);
+console.log(entriesArray);
+
+const keysArray = Object.keys(carModel);
+console.log(keysArray);
+
+const valuesArray = Object.values(carModel);
+console.log(valuesArray);
